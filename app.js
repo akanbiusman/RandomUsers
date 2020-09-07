11a30667241690198264772e20675eb782a8fcc2
usman <akanbiusman5@gmail.com>
2020-09-07 14:02:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var url = "https://randomuser.me/api/";
+
+axios
+  .get(url)
+  .then((res) => {
+    const data = res.data.results[0];
+    const name = res.data.results[0].name;
+    const gender = res.data.results[0].gender;
+    const location = res.data.results[0].location;
+
+    document.getElementById("user").src = data.picture.large;
+    document.getElementById(
+      "username"
+    ).innerText = `${name.title} ${name.last} ${name.first}`;
+    document.getElementById(
+      "info"
+    ).innerText = `${location.state}, ${location.country}`;
+
+    console.log(data);
+  })
+  .catch((err) => console.error("Something went wrong"));
